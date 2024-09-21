42ec4351f33d3eaf77c9dfad6d07c54329ab3621
dhruv <dp3903@gmail.com>
2024-09-21 16:30:38 +0530

    live chat enabled(probably,hopefully) part 6.

--- server/index.js
+++ server/index.js
@@ -8,25 +8,13 @@ const server = http.createServer(app);
 const allowedOrigins = ["http://localhost:5173", "http://127.0.0.1:5173"];
 
 const corsOptions = {
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1 || (origin.endsWith('-dp3903s-projects.vercel.app/') && origin.startsWith('https://anonymous-chatroom-client-'))) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: '*',
     methods: ['GET', 'POST'],
 };
 
 const io = socketIO(server,{
     cors: {
-        origin: (origin, callback) => {
-            if (!origin || allowedOrigins.indexOf(origin) !== -1 || (origin.endsWith('-dp3903s-projects.vercel.app/') && origin.startsWith('https://anonymous-chatroom-client-'))) {
-              callback(null, true);
-            } else {
-              callback(new Error('Not allowed by CORS'));
-            }
-        },
+        origin: '*'
     }
 });
 
