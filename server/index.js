bdff9998df91b0ed4c4776506cdf2d27161799e4
Dhruv R Patel <122767649+dp3903@users.noreply.github.com>
2024-10-01 09:17:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,13 +6,13 @@ const app = express();
 const server = http.createServer(app);
 
 const corsOptions = {
-    origin: '*',
+    origin: 'https://anonymous-chatroom-client.vercel.app',
     methods: ['GET', 'POST'],
 };
 
 const io = socketIO(server,{
     cors: {
-        origin: '*',
+        origin: 'https://anonymous-chatroom-client.vercel.app',
         methods: ["GET", "POST"],
         transports: ['websocket', 'polling']    // Fall back to polling if WebSocket fails
     }
